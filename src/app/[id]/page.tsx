2c7af48418cd935a64ac1ce91df6372087036bb0
yukitake <koshi.yukitake@gmail.com>
2023-08-22 15:15:24 +0900

    uidでLocationを検索するように変更

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -15,7 +15,7 @@ const zoom = 13;
 export default function Preview({
     params: { id },
 }: {
-    params: { id: number };
+    params: { id: string };
 }) {
 
     const [location, setLocation] = useState<Location | null>(null);
@@ -31,10 +31,10 @@ export default function Preview({
         lng: center_longitude,
     };
 
-    async function getLocation(id:number) {
+    async function getLocation(id:string) {
         const response = await axios.get(`/api/location/${id}`)
         if(response.data.status == 404) {
-            window.location.href = ''
+            window.location.href = '/'
         }
         const location = response.data.location
         setLocation(location)
@@ -53,7 +53,7 @@ export default function Preview({
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center px-12 py-4">
+        <>
             <head>
                 <script async src="https://platform.twitter.com/widgets.js"></script>
                 <meta property="twitter:card" content="summary_large_image" />
@@ -65,44 +65,45 @@ export default function Preview({
                 <meta property="og:image" content={location?.thumbnail} />
                 <meta property="twitter:image" content={location?.thumbnail} />
             </head>
-
-            <div className="flex flex-col items-center justify-center h-screen">
-                <div className="flex items-center justify-between w-full">
-                    <a className='font-serif text-4xl' href="/">Locatify</a>
-                    <a className="twitter-share-button" href="https://twitter.com/intent/tweet"></a>
-                </div>
-                {location != null && 
-                    <div>
-                        <div className='text-xl flex items-center justify-start title-h'>
-                            <p>{location.track_name}</p>
-                        </div>
-                        <div className='flex items-center justify-end title-h'>
-                            <p>By: {location.artist_name}</p>
-                        </div>
-                        <div className="flex flex-col items-center justify-center">
-                            <img className='w-full' src={location.thumbnail} alt="" />
-                            <audio className='my-3 audio-h w-full' controls src={location.preview_url}></audio>
-                        </div>
+            <main className="flex min-h-screen flex-col items-center justify-center px-12 py-4">
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <div className="flex items-center justify-between w-full">
+                        <a className='font-serif text-4xl' href="/">Locatify</a>
+                        <a className="twitter-share-button" href="https://twitter.com/intent/tweet"></a>
                     </div>
-                }
-            </div>
+                    {location != null && 
+                        <div>
+                            <div className='text-xl flex items-center justify-start title-h'>
+                                <p>{location.track_name}</p>
+                            </div>
+                            <div className='flex items-center justify-end title-h'>
+                                <p>By: {location.artist_name}</p>
+                            </div>
+                            <div className="flex flex-col items-center justify-center">
+                                <img className='w-full' src={location.thumbnail} alt="" />
+                                <audio className='my-3 audio-h w-full' controls src={location.preview_url}></audio>
+                            </div>
+                        </div>
+                    }
+                </div>
 
-            <div className="flex flex-col items-start justify-start w-full">
-                <GoogleMap 
-                    mapContainerStyle={containerStyle} 
-                    center={center} 
-                    zoom={zoom}
-                >
-                    {center_latitude != 0 && <Marker 
-                        position={center} 
-                        label=''
-                        icon={{
-                            url: '/music.svg',
-                            scaledSize: new google.maps.Size(40,40)
-                        }}
-                    />}
-                </GoogleMap>
-            </div>
-        </main>
+                <div className="flex flex-col items-start justify-start w-full">
+                    <GoogleMap 
+                        mapContainerStyle={containerStyle} 
+                        center={center} 
+                        zoom={zoom}
+                    >
+                        {center_latitude != 0 && <Marker 
+                            position={center} 
+                            label=''
+                            icon={{
+                                url: '/music.svg',
+                                scaledSize: new google.maps.Size(40,40)
+                            }}
+                        />}
+                    </GoogleMap>
+                </div>
+            </main>
+        </>
     )
 }
