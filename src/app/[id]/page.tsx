263ef618e7793719353e3ca4a37b46c1b506e6a5
yukitake <koshi.yukitake@gmail.com>
2023-08-24 09:27:07 +0900

    redirect処理を修正

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,16 +1,7 @@
 import axios from 'axios';
-import { PostComponent, PostProps } from '@/components/post';
+import { PostComponent } from '@/components/post';
 import type { Metadata, ResolvingMetadata } from 'next'
-
-async function getLocation(id:string) {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/location/${id}`)
-    if(response.data.status == 404) {
-        window.location.href = '/'
-    }
-    const location = response.data.location
-
-    return location
-}
+import { redirect } from 'next/navigation';
 
 export default async function PostPage({ params: { id }, }: {params: { id: string };}){
 
@@ -19,10 +10,20 @@ export default async function PostPage({ params: { id }, }: {params: { id: strin
     return <PostComponent location={location} />;
 };
 
+async function getLocation(id:string) {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/location/${id}`)
+    if(response.data.status == 404) {
+        redirect('/')
+    }
+    const location = response.data.location
+
+    return location
+}
+
 export async function generateMetadata(
     { params: { id }, }: {params: { id: string };},
     parent?: ResolvingMetadata
-  ): Promise<Metadata> {
+): Promise<Metadata> {
     
     const location = await getLocation(id);
 
