b4c11741a66a9f3c7416bdf921d91ba072e96b4f
yukitake <koshi.yukitake@gmail.com>
2023-08-28 10:51:04 +0900

    サービス説明ページの作成

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,19 @@ export default function About(){
     return <>
         <main className="px-12 py-4">
             <div className='flex flex-col items-center justify-center'>
-                <span className="font-serif text-4xl">Locatify</span>
+                <h1 className="font-serif text-4xl">Locatify</h1>
+                <div className="my-5">
+                    <h3 className="font-serif text-3xl">Discovering Serenity in Beloved Places, Harmonized by Favorite Melodies.</h3>
+                </div>
+                <div className="my-5">
+                    <p>Locatifyはあなたの好きな曲と場所をポスト・シェアすることで、今の気持ちを表現することができます。</p>
+                </div>
+                <div className="my-5">
+                    <p>他ユーザのポストから、いつも通るあの場所や有名な観光地の楽しみかたが変わるかもしれません。</p>
+                </div>
+                <div className="my-5">
+                    <p>周りを見渡して、あなたの新しい音楽をさがしてみましょう！！</p>
+                </div>
             </div>
         </main>
     </>
